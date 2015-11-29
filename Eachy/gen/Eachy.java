c2e23f5f25d9a320f5e5facaebad413b09e992c1
robtreacycollege <robtreacywork@gmail.com>
2015-11-29 21:50:25 +0000

    Eachy
    Eachy

---
+++ Eachy/gen/Eachy.java
@@ -0,0 +1,64 @@
+/**
+ * GENERATED CODE - DO NOT CHANGE
+ */
+
+import astra.acre.*;
+import astra.cartago.*;
+import astra.core.*;
+import astra.event.*;
+import astra.messaging.*;
+import astra.formula.*;
+import astra.lang.*;
+import astra.eis.*;
+import astra.statement.*;
+import astra.term.*;
+import astra.type.*;
+import astra.tr.*;
+import astra.util.*;
+
+public class Eachy extends ASTRAClass {
+	public Eachy() {
+		setParents(new Class[] {astra.lang.Agent.class});
+		addRule(new Rule(
+			new GoalEvent('+',
+				new Goal(
+					new Predicate("main", new Term[] {
+						new Variable(Type.LIST, "args")
+					})
+				)
+			),
+			Predicate.TRUE,
+			new Block(
+				"Eachy", new int[] {2,27,4,5},
+				new Statement[] {
+				}
+			)
+		));
+	}
+
+	public void initialize(astra.core.Agent agent) {
+	}
+
+	public Fragment createFragment(astra.core.Agent agent) throws ASTRAClassNotFoundException {
+		Fragment fragment = new Fragment(this);
+		return fragment;
+	}
+
+	public static void main(String[] args) {
+		ListTerm argList = new ListTerm();
+		for (String arg: args) {
+			argList.add(Primitive.newPrimitive(arg));
+		}
+
+		String name = java.lang.System.getProperty("astra.name", "main");
+		try {
+			astra.core.Agent agent = new Eachy().newInstance(name);
+			agent.initialize(new Goal(new Predicate("main", new Term[] { argList })));
+			Scheduler.schedule(agent);
+		} catch (AgentCreationException e) {
+			e.printStackTrace();
+		} catch (ASTRAClassNotFoundException e) {
+			e.printStackTrace();
+		};
+	}
+}
